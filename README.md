eb7087a24d20d716e3e03343d5dd04488458f52d
Marin County Breeding Bird Atlas <77134855+mcbba@users.noreply.github.com>
2024-12-31 14:26:30 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A story map for the Marin County Breeding Bird Atlas based of off code by leaflet-storymap. Google forms connectivity added for this project. The JS file here has been heavily altered for our project needs, would reccomend if you are trying to replicate this map to use the original leaflet-storymap JS.
---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xiVsJyZgNXziuB4ZcCw-_Pww6gfLpF3wsrYWIh6dLp4/edit#gid=0';
+//var googleApiKey = 'AIzaSyCq3m304-zo15BPsOtlR3BnHsClkaCYBxg';
+var googleApiKey = 'AIzaSyDyMP61oufYkit234gesNAqw5laC8gXGmY';
+ //2023 old key 'AIzaSyCZ09Fj7ts6rveKVlWuC3poNNgQjIZySWM';
+// new key for 2022: AIzaSyAJ2ThY0Xqtjl4MXohW-AhgqwfrctgT5zw
